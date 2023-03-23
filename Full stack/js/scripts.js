bca03618b24d9760b586d12ee46628aac93c335d
Andrew Maliszko <120054782+AndrewM135@users.noreply.github.com>
2023-03-23 17:40:28 -0400

    Delete Full Stack
    Making changes to Full stack

--- Full stack/js/scripts.js
+++
@@ -1,6 +0,0 @@
-let pokemmonList = [{name:'Eevee', height:'0.3m', weight:'6.5kg', type:'normal'},
-                    {name:'Umbreon', height:'1m', weight:'27kg', type:'dark'},
-                    {name:'Flareon', height:'0.9m', weight:'25kg', type:'fire'},
-                    {name:'Glaceon', height:'0.8m', weight:'25.9kg', type:'ice'},
-                    {name:'Leafeon', height:'1m', weight:'25.5kg', type:'grass'},
-                    {name:'Sylveon', height:'1m', weight:'23.5kg', type:'fairy'} ]
